987f8f76f18baa0a867db31ccc81049288affb91
dinusha <dnsw83@gmail.com>
2017-10-13 00:05:59 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  status:boolean = false;
+  addStyle() {
+    this.status = !this.status;
+    console.log('status:', this.status);
+  }
 }
